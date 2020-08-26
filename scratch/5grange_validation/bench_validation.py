dd7ae7e5ce70c68235c15687df9744e07b1ef840
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2020-08-26 12:07:03 -0300

    Add missing NS3 logging parameter and aggregate plots

--- scratch/5grange_validation/bench_validation.py
+++ scratch/5grange_validation/bench_validation.py
@@ -2,8 +2,10 @@ import os
 import matplotlib
 import shutil
 import multiprocessing as mp
-from plot_mcs_distribution import plot_mcs_dist as plot_mcs
-from plot_snr_distribution import plot_snr_dist as plot_snr
+from plot_mcs_distribution import plot_mcs_dist as extract_n_plot_mcs
+from plot_mcs_distribution import plot_mcs_hist as plot_mcs
+from plot_snr_distribution import plot_snr_hist as plot_snr
+from plot_snr_distribution import plot_snr_dist as extract_n_plot_snr
 
 cwd = os.path.abspath(os.getcwd())
 base_dir = "../../build/bin/"
@@ -17,45 +19,41 @@ def exec_sim_parameters(*arg_tuple):
     channel_model = arg_tuple[1]
     distance = arg_tuple[2]
     folder = arg_tuple[3]
-    if os.path.exists(folder+"sinr.png"):
-        return True
 
-    try:
-        #Execute simulation
-        cmd = "bash -c"
-        cmd += " \"NS_LOG=LteAmc"
-        cmd += " ../../../../5grange_cdl_test"
-        cmd += " --forceMaxMcsSched=" + ("true" if forceMaxMcs else "false")
-        cmd += " --useCdlPathLoss=" + ("true" if channel_model == "CDL" else "false")
-        cmd += " --distance=%s" % (distance*1000)
-        cmd += " \""
-        simProc = subprocess.run(cmd,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.STDOUT,
-                                 cwd=folder
-                                 )
+    if not os.path.exists(folder+"sinr.png"):
+        try:
+            #Execute simulation
+            cmd = "bash -c"
+            cmd += " \"NS_LOG=LteAmc:LteSpectrumPhy"
+            cmd += " ../../../../5grange_cdl_test"
+            cmd += " --forceMaxMcsSched=" + ("true" if forceMaxMcs else "false")
+            cmd += " --useCdlPathLoss=" + ("true" if channel_model == "CDL" else "false")
+            cmd += " --distance=%s" % (distance*1000)
+            cmd += " \""
+            simProc = subprocess.run(cmd,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT,
+                                     cwd=folder
+                                     )
 
-        with open(folder+"out%dkm.txt" % distance, "w") as file:
-            file.write(simProc.stdout.decode("utf-8"))
+            with open(folder+"out%dkm.txt" % distance, "w") as file:
+                file.write(simProc.stdout.decode("utf-8"))
 
-        throughputProc = subprocess.run(["python",
-                                         cwd+os.sep+"get_thr.py"
-                                         ],
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.STDOUT,
-                                        cwd=folder
-                                        )
+            throughputProc = subprocess.run(["python",
+                                             cwd+os.sep+"get_thr.py"
+                                             ],
+                                            stdout=subprocess.PIPE,
+                                            stderr=subprocess.STDOUT,
+                                            cwd=folder
+                                            )
 
-        with open(folder+"perf.txt", "w") as file:
-            file.write(throughputProc.stdout.decode("utf-8"))
-
-        plot_mcs([distance], base_dir=folder, cmd=True)
-        plot_snr([distance], base_dir=folder, cmd=True)
-
-    except Exception:
-        return False
-    finally:
-        return True
+            with open(folder+"perf.txt", "w") as file:
+                file.write(throughputProc.stdout.decode("utf-8"))
+        except Exception:
+            return False, None, None
+    mcs = extract_n_plot_mcs([distance], base_dir=folder, cmd=True)
+    snr = extract_n_plot_snr([distance], base_dir=folder, cmd=True)
+    return True, mcs, snr
 
 
 def make_dir(dir):
@@ -89,10 +87,43 @@ if __name__ == "__main__":
     del distance, distanceFolder, channel_model, channelFolder, forceMaxMcs, maxMcsFolder
 
     # Run simulation programs and scripts to process results
+    results = None
     with mp.Pool(processes=10) as p:
-        p.starmap(func=exec_sim_parameters, iterable=thread_parameters)
+        results = p.starmap(func=exec_sim_parameters, iterable=thread_parameters)
 
-    # Todo: aggregate results in a useful manner
+    # Aggregate results in a useful manner
+    for forceMaxMcs in [False, True]:
+        maxMcsFolder = output_dir+"ForceMaxMcs_%s" % str(forceMaxMcs) + os.sep
+        for channel_model in channel_models:
+            channelFolder = maxMcsFolder + channel_model + os.sep
+            mcs_freq_per_d = {}
+            snr_freq_per_d = {}
+
+            # Aggregate results for different distances
+            for distance in distances:
+                finished, mcs_freq, snr_freq = results.pop(0)
+                mcs_freq_per_d.update(mcs_freq)
+                snr_freq_per_d.update(snr_freq)
+
+            # Create output figures
+            import matplotlib.pyplot as plt
+            fig, axis = plt.subplots(nrows=len(mcs_freq_per_d))
+            if len(distances) == 1:
+                axis = [axis]
+
+            fig2, axis2 = plt.subplots(nrows=len(snr_freq_per_d))
+            if len(distances) == 1:
+                axis2 = [axis2]
+
+            # Plot aggregated results
+            axis[-1].set_xlabel("MCS")
+            axis2[-1].set_xlabel("SNR")
+            plot_mcs(axis, mcs_freq_per_d)
+            plot_snr(axis2, snr_freq_per_d)
+
+            # Save results to output files
+            fig.savefig(channelFolder+"mcs.png")
+            fig2.savefig(channelFolder+"snr.png")
 
 
 
