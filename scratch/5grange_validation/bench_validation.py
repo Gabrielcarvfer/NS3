700e7e14c3114acb11102804f4afbfbc9f63196e
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2020-08-27 15:15:17 -0300

    Add plots for corrupted transport blocks and received throughput

--- scratch/5grange_validation/bench_validation.py
+++ scratch/5grange_validation/bench_validation.py
@@ -1,9 +1,11 @@
 import os
+import random
 import re
 import matplotlib
 import shutil
 import matplotlib.pyplot as plt
 import multiprocessing as mp
+import json
 from plot_mcs_distribution import plot_mcs_dist as extract_n_plot_mcs
 from plot_mcs_distribution import plot_mcs_hist as plot_mcs
 from plot_snr_distribution import plot_snr_hist as plot_snr
@@ -12,7 +14,7 @@ from plot_snr_distribution import plot_snr_dist as extract_n_plot_snr
 cwd = os.path.abspath(os.getcwd())
 base_dir = "../../build/bin/"
 output_dir = os.path.abspath(base_dir) + "/range_bench_validation/"
-
+plt.ioff() # disable interactive plotting
 
 # Spawn processes passing the arguments for each simulation
 def exec_sim_parameters(*arg_tuple):
@@ -49,7 +51,7 @@ def exec_sim_parameters(*arg_tuple):
                                             cwd=folder
                                             )
 
-            with open(folder+"perf.txt", "w") as file:
+            with open(folder+"perf.txt", "w", encoding="utf-8") as file:
                 file.write(throughputProc.stdout.decode("utf-8"))
         except Exception:
             return False, None, None
@@ -65,16 +67,21 @@ def make_dir(dir):
         pass
 
 
+def randcolor():
+    return random.random(), random.random(), random.random()
+
+
 if __name__ == "__main__":
     mp.freeze_support()
-    channel_models = ("CDL", "RANGE5G")
+    channel_models = ("RANGE5G", )#"CDL")
+    forcedMaxMcs = (True,)
     distances = [1, 5, 10, 20, 35, 50, 100, ]
 
     thread_parameters = []
 
     # Create folders to dump simulation results
     make_dir(output_dir)
-    for forceMaxMcs in [False, True]:
+    for forceMaxMcs in forcedMaxMcs:
         maxMcsFolder = output_dir+"ForceMaxMcs_%s" % str(forceMaxMcs) + os.sep
         make_dir(maxMcsFolder)
         for channel_model in channel_models:
@@ -94,8 +101,17 @@ if __name__ == "__main__":
         results = p.starmap(func=exec_sim_parameters, iterable=thread_parameters)
 
     # Aggregate results in a useful manner
-    for forceMaxMcs in [False, True]:
+    for forceMaxMcs in forcedMaxMcs:
         maxMcsFolder = output_dir+"ForceMaxMcs_%s" % str(forceMaxMcs) + os.sep
+
+        # Create output figure for corruption rate plots
+        fig3, axis3 = plt.subplots()
+        axis3 = [axis3]
+
+        # Create output figure for throughput plots
+        fig4, axis4 = plt.subplots()
+        axis4 = [axis4]
+
         for channel_model in channel_models:
             channelFolder = maxMcsFolder + channel_model + os.sep
             mcs_freq_per_d = {}
@@ -105,8 +121,11 @@ if __name__ == "__main__":
             for distance in distances:
                 # Pop MCS and SNR distribution results
                 finished, mcs_freq, snr_freq = results.pop(0)
-                mcs_freq_per_d.update(mcs_freq)
-                snr_freq_per_d.update(snr_freq)
+                if finished:
+                    mcs_freq_per_d.update(mcs_freq)
+                    snr_freq_per_d.update(snr_freq)
+                else:
+                    print(channelFolder, distance, "km failed")
 
             # Create output figures for MCS and SNR
             fig, axis = plt.subplots(nrows=len(mcs_freq_per_d))
@@ -131,20 +150,49 @@ if __name__ == "__main__":
             del fig, fig2, axis, axis2, mcs_freq_per_d, snr_freq_per_d, distance, finished, mcs_freq, snr_freq
 
 
-            # Aggregate results for different distances
+            # Aggregate throughput rate results for different distances
+            received_throughput_per_d = {}
+            for distance in distances:
+                distanceFolder = channelFolder + str(distance) + "km" + os.sep
+
+                # Open perf.txt and scan for received throughput
+                contents = None
+                with open(distanceFolder + "perf.txt", "r", encoding="utf-8") as file:
+                    contents = file.readlines()
+
+                PHY_DL_received_throughput_regex = re.compile("DL Vazão: (.*)mbps")
+                #PHY_DL_injected_throughput_regex = re.compile("Vazão esperada PHY: (.*)mbps")
+
+                regex = None
+                throughput = 0.0
+                for line in contents:
+                    regex = PHY_DL_received_throughput_regex.match(line)
+                    if regex is None:
+                        continue
+                    throughput = regex.groups()[0]
+                    throughput = float(throughput)
+                received_throughput_per_d[distance] = throughput
+                del regex, line, contents, throughput, file
+
+            # Plot throughput for a given channel model
+            axis4[0].plot(list(received_throughput_per_d.keys()), list(received_throughput_per_d.values()), label=channel_model, color=randcolor())
+
+
+            # Aggregate corruption rate results for different distances
             corrupted_freq_per_d = {}
             for distance in distances:
                 distanceFolder = channelFolder + str(distance) + "km" + os.sep
 
                 # Open out__km.txt and scan for corrupted transport blocks to calculate the error rate
                 contents = None
-                with open("out%dkm.txt" % distance, "r") as file:
+                with open(distanceFolder + "out%dkm.txt" % distance, "r") as file:
                     contents = file.readlines()
 
                 corrupted_regex = re.compile(".* corrupted (.*).*")
                 corrupted_TBs = 0
                 total_TBs = 0
 
+                regex = None
                 for line in contents:
                     regex = corrupted_regex.match(line)
                     if regex is None:
@@ -153,10 +201,30 @@ if __name__ == "__main__":
                     corrupted = regex.groups()[0]
                     corrupted = int(corrupted)
                     corrupted_TBs += corrupted
-                corrupted_freq_per_d[distance] = (corrupted_TBs, total_TBs)
+                corrupted_freq_per_d[distance] = corrupted_TBs/total_TBs*100
                 del regex, line, contents, corrupted, file
-            del corrupted_TBs, total_TBs, contents
+            del corrupted_TBs, total_TBs
+
+            # Plot corruption rate plots for a given channel model
+            axis3[0].plot(list(corrupted_freq_per_d.keys()), list(corrupted_freq_per_d.values()), label=channel_model, color=randcolor())
+
+        # Set axis labels for corruption rate plot
+        axis3[-1].set_xlabel("Distance (km)")
+        axis3[-1].set_ylabel("Corruption rate (%)")
+        axis3[-1].legend()
+
+        # Save results to output files for corruption rate
+        fig3.savefig(maxMcsFolder+"corruption_rate.png")
+
+        # Set axis labels for corruption rate plot
+        axis4[-1].set_xlabel("Distance (km)")
+        axis4[-1].set_ylabel("Received throughput (mbps)")
+        axis4[-1].legend()
+
+        # Save results to output files for corruption rate
+        fig4.savefig(maxMcsFolder+"throughput.png")
 
 
-
-
+        pass
+    pass
+    # end main
