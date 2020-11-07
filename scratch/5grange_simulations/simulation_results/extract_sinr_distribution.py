9ba1a1447ebeeb845c6a691b8ee0848c8931c29a
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2020-11-06 23:53:02 -0300

    Remove custom performance extraction scripts

--- scratch/5grange_simulations/simulation_results/extract_sinr_distribution.py
+++
@@ -1,6 +0,0 @@
-from simulation_results.extract_lte_spectrum_phy_results import LteSpectrumPhyResults
-
-
-def extract_sinr_distribution(simulation_path):
-    LteSpectrumPhyResults(simulation_path)
-    return LteSpectrumPhyResults.sinr_distribution
--- scratch/5grange_simulations/simulation_results/extract_tbler_results.py
+++
@@ -1,6 +0,0 @@
-from simulation_results.extract_lte_spectrum_phy_results import LteSpectrumPhyResults
-
-
-def extract_tbler_results(simulation_path):
-    LteSpectrumPhyResults(simulation_path)
-    return LteSpectrumPhyResults.tbler_results
