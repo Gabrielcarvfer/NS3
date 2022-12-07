5af44eeff4f4d98d5ab681d78b9905243e442fe5
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2022-12-07 17:49:37 -0300

    oran: update xAppHandoverMlpackKmeans to trigger handovers when requested

--- src/oran/model/xAppHandoverMlpackKmeans.h
+++ src/oran/model/xAppHandoverMlpackKmeans.h
@@ -8,16 +8,27 @@ namespace ns3
     class xAppHandoverMlpackKmeans : public xAppHandover
     {
       public:
-        xAppHandoverMlpackKmeans(bool kmeansEmptyPolicy = false);
+        xAppHandoverMlpackKmeans(bool kmeansEmptyPolicy = false,
+                                 float clusteringPeriodicitySec = 1.0,
+                                 bool initiateHandovers = false);
         void HandoverDecision(Json& payload);
         void HandoverSucceeded(std::string context, uint64_t imsi, uint16_t cellid, uint16_t rnti);
         void HandoverFailed(std::string context, uint64_t imsi, uint16_t cellid, uint16_t rnti);
+        void HandoverStarted (std::string context,
+                              uint64_t imsi,
+                              uint16_t cellid,
+                              uint16_t rnti,
+                              uint16_t targetCellId);
         void PeriodicClustering ();
       private:
         std::deque<std::tuple<uint16_t, uint16_t, uint16_t>> m_decision_history;
         std::map<uint16_t, uint16_t> m_rntiToClusteredCellId;
         std::map<uint16_t, uint16_t> m_rntiToCurrentCellId;
         bool m_kmeansKeepEmptyPolicy;
+        float m_clusteringPeriodicitySec;
+        bool m_initiateHandovers;
+        std::map<uint16_t, uint16_t> m_imsiInHandover;
+
     };
 }
 #endif// NS3_XAPP_HANDOVER_MLPACK_KMEANS_H
