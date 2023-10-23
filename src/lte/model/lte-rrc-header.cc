c06ddb702ac9c83cde39399cab05d5633dd79f19
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2023-10-23 14:39:32 -0300

    lte: initialize variables

--- src/lte/model/lte-rrc-header.cc
+++ src/lte/model/lte-rrc-header.cc
@@ -5890,7 +5890,7 @@ RrcConnectionReconfigurationHeader::SetMessage(LteRrcSap::RrcConnectionReconfigu
 LteRrcSap::RrcConnectionReconfiguration
 RrcConnectionReconfigurationHeader::GetMessage() const
 {
-    LteRrcSap::RrcConnectionReconfiguration msg;
+    LteRrcSap::RrcConnectionReconfiguration msg{};
 
     msg.rrcTransactionIdentifier = m_rrcTransactionIdentifier;
     msg.haveMeasConfig = m_haveMeasConfig;
