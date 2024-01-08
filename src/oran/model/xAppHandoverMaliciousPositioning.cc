10e8cc0f64248f4766f5dd6096eeec31837f3b29
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2024-01-08 17:26:41 +0100

    oran: add simulation scenario and malicious xApp

---
+++ src/oran/model/xAppHandoverMaliciousPositioning.cc
@@ -0,0 +1,179 @@
+#include "xAppHandoverMaliciousPositioning.h"
+
+#include "ns3/E2AP.h"
+#include "ns3/core-module.h"
+#include "ns3/lte-enb-net-device.h"
+#include "ns3/mobility-model.h"
+
+#include <algorithm>
+
+using namespace ns3;
+using namespace oran;
+
+NS_LOG_COMPONENT_DEFINE("xAppHandoverMaliciousPositioning");
+
+xAppHandoverMaliciousPositioning::xAppHandoverMaliciousPositioning(bool useRnti)
+    : xAppHandover(),
+      m_useRnti(useRnti)
+{
+    NS_LOG_FUNCTION(this);
+
+    /* Retrieve position of eNBs/gNBs
+     *
+     * While someone will definitely complain that I only can do this in a simulation,
+     * the reality of it is that base station locations are not a secret in any way.
+     *
+     * In fact, there are publicly accessible databases/maps with the location of many stations.
+     *
+     * You can also go in person and make the measurements by yourself using a phone and its GPS.
+     */
+
+    // Search nodes with LteEnbNetDevice installed
+    for (unsigned nodeId = 0; nodeId < NodeList::GetNNodes(); nodeId++)
+    {
+        auto node = NodeList::GetNode(nodeId);
+        for (unsigned deviceId = 0; deviceId < node->GetNDevices(); deviceId++)
+        {
+            auto lteEnbNetDevice = node->GetDevice(deviceId)->GetObject<LteEnbNetDevice>();
+            if (!lteEnbNetDevice)
+                continue;
+            auto mobilityModel = node->GetObject<MobilityModel>();
+            if (!mobilityModel)
+                NS_FATAL_ERROR("And it was at this moment, that he knew he f**kd up...");
+            m_eNbPositions[lteEnbNetDevice->GetCellId()] = mobilityModel->GetPosition();
+        }
+
+    }
+    Simulator::Schedule(Seconds(1), &xAppHandoverMaliciousPositioning::PeriodicPositioning, this);
+};
+
+std::map<uint16_t, double>
+xAppHandoverMaliciousPositioning::GetRntiRsrqMeasurements(uint16_t rnti)
+{
+    NS_LOG_FUNCTION(this);
+
+    E2AP* ric = (E2AP*)static_cast<const E2AP*>(E2AP::RetrieveInstanceWithEndpoint("/E2Node/0"));
+    std::map<uint16_t, uint16_t> rntis;
+    std::array<std::string, 4> kpmMetrics = {"/KPM/HO.SrcCellQual.RSRP",
+                                             //"/KPM/HO.SrcCellQual.RSRQ",
+                                             "/KPM/HO.TrgtCellQual.RSRP",
+                                             //"/KPM/HO.TrgtCellQual.RSRQ"
+                                            };
+
+    std::map<uint16_t, double> rsrq_measurements;
+
+    // Collate data into an armadillo matrix for processing
+    for (auto kpmMetric : kpmMetrics)
+    {
+        auto metricMap = ric->QueryKpmMetric(kpmMetric);
+
+        if (metricMap.size() == 0)
+        {
+            continue;
+        }
+
+        for (auto& e2nodeMeasurements : metricMap)
+        {
+            std::string mostRecentTimestamp("");
+            for (auto& measurementDeque : e2nodeMeasurements.second)
+            {
+                if (mostRecentTimestamp == "")
+                {
+                    mostRecentTimestamp = measurementDeque.timestamp;
+                }
+                if (mostRecentTimestamp != measurementDeque.timestamp)
+                {
+                    // Skip old measurements
+                    continue;
+                }
+                if (rnti != measurementDeque.measurements["RNTI"])
+                {
+                    // Skip rntis that do not match the requesting rnti
+                    continue;
+                }
+                if (kpmMetric == "/KPM/HO.SrcCellQual.RSRP")
+                {
+                    uint16_t cellId = measurementDeque.measurements["CELLID"];
+                    cellId++;
+                    if (rsrq_measurements.find(cellId) == rsrq_measurements.end())
+                    {
+                        rsrq_measurements[cellId] = measurementDeque.measurements["VALUE"];
+                    }
+                }
+                else
+                {
+                    uint16_t cellId = measurementDeque.measurements["TARGET"];
+                    if (rsrq_measurements.find(cellId) == rsrq_measurements.end())
+                    {
+                        rsrq_measurements[cellId] = measurementDeque.measurements["VALUE"];
+                    }
+                }
+            }
+        }
+    }
+
+    return rsrq_measurements;
+}
+
+Vector3D
+xAppHandoverMaliciousPositioning::Multilateration(std::map<uint16_t, double>& measurements)
+{
+    Vector3D position;
+
+    if (measurements.size()>=3)
+    {
+        auto it = measurements.begin();
+        // Collect measured power (dBm) and position of respective cells
+        auto Pa = it->second;
+        Vector3D A = m_eNbPositions.at((it++)->first);
+
+        auto Pb = it->second;
+        Vector3D B = m_eNbPositions.at((it++)->first);
+
+        auto Pc = it->second;
+        Vector3D C = m_eNbPositions.at((it++)->first);
+
+        // Use FSPL to estimate the distance to the cells
+        // based on the received power in dbm
+
+        // Calculate intermediate steps
+        auto E = 2*(-A.x+B.x);
+        auto F = 2*(-A.y+B.y);
+        auto G = pow(Pa,2)-pow(Pb,2)+pow(A.y, 2)-pow(B.y, 2);
+        auto H = 2*(-B.x+C.x);
+        auto I = 2*(-B.y+C.y);
+        auto J = pow(Pb,2)-pow(Pc,2)+pow(B.y, 2)-pow(C.y, 2);
+
+        position.x = (G*I-J*F)/(I*E-F*H);
+        position.y = (G*H-E*J)/(F*H-E*I);
+    }
+    return position;
+}
+
+void
+xAppHandoverMaliciousPositioning::PeriodicPositioning()
+{
+    NS_LOG_FUNCTION(this);
+
+    // Use positioning techniques to locate UEs
+    // Notice: rnti can be legitimate or random
+    for (auto rnti: m_rntiList)
+    {
+        auto measurements = GetRntiRsrqMeasurements(rnti);
+        auto estimated_position = Multilateration(measurements);
+        //std::cout << estimated_position << std::endl;
+    }
+
+    // Re-schedule this function
+    Simulator::Schedule(Seconds(1), &xAppHandoverMaliciousPositioning::PeriodicPositioning, this);
+}
+
+void
+xAppHandoverMaliciousPositioning::HandoverDecision(Json& payload)
+{
+    NS_LOG_FUNCTION(this);
+    // only collect RNTI from request
+    m_rntiList.push_back(payload["RNTI"]);
+
+    // does nothing on purpose
+}
---
+++ src/oran/model/xAppHandoverMaliciousPositioning.h
@@ -0,0 +1,52 @@
+#ifndef NS3_XAPP_HANDOVER_MALICIOUS_POSITIONING_H
+#define NS3_XAPP_HANDOVER_MALICIOUS_POSITIONING_H
+
+#include "ns3/xAppHandover.h"
+#include "ns3/vector.h"
+
+/**
+ * \ingroup oran
+ * ns3::oran::xAppHandoverMaliciousPositioning declaration.
+ */
+
+namespace ns3
+{
+namespace oran
+{
+/**
+ * \ingroup oran
+ * \class xAppHandoverMaliciousPositioning
+ * \brief An implementation of a Handover xApp using
+ * the MaliciousPositioning strategy
+ */
+class xAppHandoverMaliciousPositioning : public xAppHandover
+{
+  public:
+    /**
+     * \brief Constructor of xAppHandoverMaliciousPositioning.
+     */
+    xAppHandoverMaliciousPositioning(bool useRnti = false);
+    /**
+     * \brief Decides whether to reject the requested handover or reform the decision.
+     * \param [in, out] payload Json payload with the UE to handover (RNTI)
+     *                  and target cell to handover (Target Primary Cell ID).
+     */
+    void HandoverDecision(Json& payload);
+    /**
+     * \brief This functions is called periodically to represent the inner workings
+     *        of a malicious xApp tracking users.
+     */
+    void PeriodicPositioning();
+
+    std::map<uint16_t, double> GetRntiRsrqMeasurements(uint16_t rnti);
+    Vector3D Multilateration(std::map<uint16_t, double>&);
+
+
+  private:
+    std::vector<uint16_t> m_rntiList;
+    std::map<uint16_t, ns3::Vector3D> m_eNbPositions;
+    bool m_useRnti = false;
+};
+} // namespace oran
+} // namespace ns3
+#endif // NS3_XAPP_HANDOVER_MALICIOUS_POSITIONING_H
