3677bd03d9a8f1e2f98cb61ed4fd97633fa36ce0
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2024-04-26 21:01:41 +0200

    a

--- src/oran/examples/PrivacySafeguardingScenarioCompanion.py
+++ src/oran/examples/PrivacySafeguardingScenarioCompanion.py
@@ -13,7 +13,10 @@ privacy_dir = os.sep.join([ns3_dir, "build", "src", "oran", "examples"])
 if not os.path.exists(privacy_dir):
     # build simulation
     subprocess.call(["ns3", "build", "PrivacySafeguardingScenario"], cwd=ns3_dir)
+    
+infraPosition = {}
 def get_ground_truth_trajectories():
+    global infraPosition
     trajectories = {}
     ground_truth_file = os.path.join(privacy_dir, "tracking.xml")
     with open(ground_truth_file, "r") as f:
@@ -32,6 +35,15 @@ def get_ground_truth_trajectories():
         for key in list(trajectories.keys()):
             if not trajectories[key]:
                 del trajectories[key]
+                infraPosition[key] = None
+        for key in infraPosition.keys():
+            initPos = list(filter(lambda x: int(x["@id"]) == key, ground_truth["anim"]["node"]))[0]
+            x,y = initPos["@locX"], initPos["@locY"]
+            x,y = int(x), int(y)
+            label = list(filter(lambda x: int(x["@id"]) == key, ground_truth["anim"]["nu"]))
+            # Trim updates with no coordinates
+            label = list(filter(lambda x: "@descr" in list(x.keys()), label))[0]["@descr"]
+            infraPosition[key] = (x, y, label)
     return trajectories
 
 def get_malicious_tracking_trajectories():
@@ -58,14 +70,17 @@ fig, axis = plt.subplots(3,3, sharex=True, sharey=True)
 for i, key in enumerate(malicious_node_trajectories):
     truth = ground_truth_node_trajectories[key]
     approx = malicious_node_trajectories[key]
+    #print(len(truth), " ", len(approx))
     xt = list(map(lambda z: float(z[0]), truth))
     yt = list(map(lambda z: float(z[1]), truth))
     xa = list(map(lambda z: float(z[0]), approx))
     ya = list(map(lambda z: float(z[1]), approx))
     row = i // 3
     col = i % 3
-    axis[row][col].scatter(xt, yt, label=f"truth for node{key}")
-    axis[row][col].scatter(xa, ya, label=f"approx for node{key}")
+    axis[row][col].scatter(xt, yt, label=f"truth for node{key}", s=0.5)
+    axis[row][col].scatter(xa, ya, label=f"approx for node{key}", s=0.5)
+    for (x,y,label) in infraPosition.values():
+        axis[row][col].scatter(x,y,label=label)
     axis[row][col].set_xlim([0, 2600])
     axis[row][col].set_ylim([0, 2600])
 plt.tight_layout()
