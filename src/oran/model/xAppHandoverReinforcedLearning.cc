e165eed45b34e117b864ffc01613b357bc494dd5
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2025-02-18 02:12:44 +0100

    Set model relative path to source files

--- src/oran/model/xAppHandoverReinforcedLearning.cc
+++ src/oran/model/xAppHandoverReinforcedLearning.cc
@@ -59,9 +59,9 @@ xAppHandoverReinforcedLearning::ChooseTargetCellId(uint16_t rnti)
         g_interpreter = new py::scoped_interpreter{};
         pyhrl = py::module_::import("HandoverRL");
 
-	fs::path cwd = fs::current_path();
-        auto load_path =  cwd / "src/oran/model/target.pth";
-        auto save_path = cwd / "src/oran/model/target2.pth";
+	fs::path cwd = fs::path(__FILE__).parent_path();
+    auto load_path = cwd / "target.pth";
+    auto save_path = cwd / "target2.pth";
 
 	pyhrl.attr("init_module")
         (   metric_buffer_len, 2,
